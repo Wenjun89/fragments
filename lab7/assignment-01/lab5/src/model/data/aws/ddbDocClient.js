cd87cac25cc1a26dc9777182ba70fac7e3e75512
Michelle Wei <michellewei0101@gmail.com>
2026-07-31 10:25:36 -0400

    fix: remove tracked .env file and complete aws integration

--- lab7/assignment-01/lab5/src/model/data/aws/ddbDocClient.js
+++ lab7/assignment-01/lab5/src/model/data/aws/ddbDocClient.js
@@ -1,19 +1,7 @@
 // src/model/data/aws/ddbDocClient.js
 const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
-
 const { DynamoDBDocumentClient } = require('@aws-sdk/lib-dynamodb');
 const logger = require('../../../logger');
-
-const getCredentials = () => {
-  if (process.env.AWS_ACCESS_KEY_ID && process.env.AWS_SECRET_ACCESS_KEY) {
-    const credentials = {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    };
-    logger.debug('Using extra DynamoDB Credentials');
-    return credentials;
-  }
-};
 
 const getDynamoDBEndpoint = () => {
   if (process.env.AWS_DYNAMODB_ENDPOINT_URL) {
@@ -28,21 +16,16 @@ const getDynamoDBEndpoint = () => {
 const ddbClient = new DynamoDBClient({
   region: process.env.AWS_DYNAMODB_REGION || process.env.AWS_REGION || 'us-east-1',
   endpoint: getDynamoDBEndpoint(),
-  credentials: getCredentials(),
 });
 
 const ddbDocClient = DynamoDBDocumentClient.from(ddbClient, {
   marshallOptions: {
-
-    convertEmptyValues: false, 
-
-    removeUndefinedValues: false, 
-  
-    convertClassInstanceToMap: true, 
+    convertEmptyValues: false,
+    removeUndefinedValues: false,
+    convertClassInstanceToMap: true,
   },
   unmarshallOptions: {
-    
-    wrapNumbers: false, 
+    wrapNumbers: false,
   },
 });
 
